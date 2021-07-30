bb2f9bad28da3908a33ddf333e25ee37417dbfa6
Takayuki Okuda <momori.nyan@gmail.com>
2021-07-30 09:51:08 +0900

    加減算のできるコンパイラ

---
+++ test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# assert関数の作成
+assert() {
+    # 引数の設定
+    expected="$1"
+    input="$2"
+
+    # アセンブリ
+    ./main "$input" > tmp.s
+    cc -o tmp tmp.s
+    ./tmp
+    actual="$?"
+
+    # 判定
+    if [ "$actual" = "$expected" ]; then
+        echo "$input => $actual"
+    else
+        echo "$input => $expected expected, but got $actual"
+        exit 1
+    fi
+}
+
+assert 0 0
+assert 42 42
+assert 21 "5+20-4"
+
+echo OK
